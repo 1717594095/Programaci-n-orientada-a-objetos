96e728e26dee103316bcb7d3d2cce6cd52552ac1
1717594095 <ca.salgadov@uea.edu.ec>
2025-03-20 14:31:50 -0500

    Trabajo Semana 12

--- Unidad 2/Sistema de Gestión de biblioteca digital.py
+++ Unidad 2/Sistema de Gestión de biblioteca digital.py
@@ -128,4 +128,3 @@ libros_prestados = biblioteca.listar_libros_prestados("001")
 print("\nLibros prestados a Juan Pérez después de la devolución:")
 for libro in libros_prestados:
     print(libro)
-
